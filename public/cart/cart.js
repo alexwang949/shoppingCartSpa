b980e1ce247ca46b8a6eb02f1e57ee562e4666b6
Alex Wang <alexwang949@gmail.com>
2015-08-20 16:21:42 -0400

    displayed options for radio buttons appropriately, next step is to calculate total for shopping cart checkout

--- public/cart/cart.js
+++ public/cart/cart.js
@@ -86,12 +86,13 @@ cartModule.directive('checkList', function() {
 	return {
 		restrict: 'E',
 		scope: {
-			option: '='
+			option: '=',
+			name: '='
 		},
 		template: function(elem, attrs) {
             return '<div class="panel-body">\
                     <div class="radio" ng-repeat="i in option">\
-                        <label><input type="radio">{{ i.size }} Rs.{{ i.price }}</label>\
+                        <label><input type="radio" name="{{ name }}">{{ i.size }} Rs.{{ i.price }}</label>\
                     </div>\
                 </div>'
 		}
